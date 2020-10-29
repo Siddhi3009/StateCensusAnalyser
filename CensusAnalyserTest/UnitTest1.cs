c0837847b0aaefa7a94bb54b1043bef29f09219e
Administrator <siddhiseth3009@gmail.com>
2020-10-30 02:36:52 +0530

    [Siddhi]Add. TC1.4 added for passing test case with incorrect delimiter in a file of StateCensusData and stateCode

--- CensusAnalyserTest/UnitTest1.cs
+++ CensusAnalyserTest/UnitTest1.cs
@@ -58,5 +58,13 @@ namespace CensusAnalyserTest
             Assert.AreEqual(CensusAnalyserException.ExceptionType.INVALID_FILE_TYPE, censusException.eType);
             Assert.AreEqual(CensusAnalyserException.ExceptionType.INVALID_FILE_TYPE, stateException.eType);
         }
+        [Test]
+        public void GivenIncorrectDelimiterCsvFile_WhenReaded_ShouldReturnCustomException()
+        {
+            var censusException = Assert.Throws<CensusAnalyserException>(() => censusAnalyser.LoadCensusData(delimiterIndianCensusFilePath, Country.INDIA, indianStateCensusHeaders));
+            var stateException = Assert.Throws<CensusAnalyserException>(() => censusAnalyser.LoadCensusData(delimiterIndianStateCodeFilePath, Country.INDIA, indianStateCodeHeaders));
+            Assert.AreEqual(CensusAnalyserException.ExceptionType.INCORRECT_DELIMITER, censusException.eType);
+            Assert.AreEqual(CensusAnalyserException.ExceptionType.INCORRECT_DELIMITER, stateException.eType);
+        }
     }
 }
