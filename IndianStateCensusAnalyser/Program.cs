7cd451a9bb524ea1df576fe8ddbbd2c3f9d861f3
Administrator <siddhiseth3009@gmail.com>
2020-10-30 01:34:59 +0530

    [Siddhi]Add. Welcome message added

---
+++ IndianStateCensusAnalyser/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IndianStateCensusAnalyser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wecome to Indian States Census Analyser Problem");
+        }
+    }
+}
